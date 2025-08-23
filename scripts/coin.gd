6b5eebe5e745fbf1cd43cc1ff3a7daf258b79d0b
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2025-08-23 19:12:55 +0700

    finished

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1")
-	queue_free()
+func _on_body_entered(_body) -> void:
+	game_manager.add_point()
+	animation_player.play("PickUpAnimation")
